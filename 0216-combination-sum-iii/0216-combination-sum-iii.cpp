06085593704c47e24ed82cddbd74d9c26befda3b
OmarShamkh <44472968+OmarShamkh@users.noreply.github.com>
2024-09-02 18:03:31 +0300

    Time: 2 ms (51.02%), Space: 9.4 MB (16.28%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -3,22 +3,21 @@ public:
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> list;
         vector<int> nums;
-        solve(list, nums, k, n);
+        solve(list, nums, 1, k, n);
         return list;
     }
 
-    void solve(vector<vector<int>>& list, vector<int> nums, int k, int n){
+    void solve(vector<vector<int>>& list, vector<int> nums,int start ,int k, int n){
         if(nums.size() > k) return;
         if(n == 0 && nums.size() == k){
             list.push_back(nums);
             return;
         }
-        int i = nums.size() == 0 ? 1 : nums.back() + 1;
-        for(;i<=9; i++){
+        for(int i=start; i<=9; i++){
             // pick
             nums.push_back(i);
             // solve
-            solve(list,nums,k,n-i);
+            solve(list,nums, i+1, k,n-i);
             // leave
             nums.pop_back();
         }
