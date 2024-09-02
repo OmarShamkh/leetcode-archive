e9b5c0f2e4a42f687a24cd6aa586e3dee43f1b10
OmarShamkh <44472968+OmarShamkh@users.noreply.github.com>
2024-09-02 16:47:32 +0300

    Time: 91 ms (76.68%), Space: 77 MB (22.4%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
@@ -2,18 +2,19 @@ class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
         long long sum = 0; int n = chalk.size();
-        sum += chalk[0];
-        vector<long> pre(n);
-        pre[0] = chalk[0];
-        for(int i=1; i<n; i++){
+        // sum += chalk[0];
+        // vector<long> pre(n);
+        // pre[0] = chalk[0];
+        for(int i=0; i<n; i++){
             sum += chalk[i];
-            pre[i] = chalk[i] + pre[i-1];
+            // pre[i] = chalk[i] + pre[i-1];
         } 
         long long mod = k % sum;
         if( mod == 0) return 0;
         for(int i=0; i<n; i++){
-            cout << pre[i] << " ";
-            if(pre[i] > mod) return i;
+            // cout << pre[i] << " ";
+            if(mod < chalk[i]) return i;
+            mod -= chalk[i];
         }
         return 0;
     }
