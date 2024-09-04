314766b5ad625655467f0693e977032e9eb7b31a
OmarShamkh <44472968+OmarShamkh@users.noreply.github.com>
2024-09-04 21:14:45 +0300

    Time: 71 ms (68.23%), Space: 38.2 MB (69.06%) - LeetHub

---
+++ 0906-walking-robot-simulation/0906-walking-robot-simulation.cpp
@@ -0,0 +1,39 @@
+class Solution {
+static const int const_hash = 60013;
+int hash(int x, int y){ 
+    return x * const_hash + y; 
+}
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        unordered_set<int> sett;
+        vector<int> cur = {0,0}; // 0 1 2 3
+        vector<vector<int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int maxd = 0;
+        int pos = 0;
+        for (auto& obstacle : obstacles) {
+            sett.insert(hash(obstacle[0], obstacle[1]));
+        }
+        for(int i=0; i<commands.size(); i++){
+            if(commands[i] == -1){
+                pos = (pos + 1) % 4;
+                continue;
+            }
+            if(commands[i] == -2){
+                pos = (pos + 3) % 4;
+                continue;
+            }
+            vector<int> direction = directions[pos];
+            for (int j = 0; j<commands[i]; j++) {
+                int nextX = cur[0] + direction[0];
+                int nextY = cur[1] + direction[1];
+                if (sett.contains(hash(nextX, nextY))) {
+                    break;
+                }
+                cur[0] = nextX;
+                cur[1] = nextY;
+            }
+            maxd = max(maxd, (cur[0]*cur[0] + cur[1]*cur[1]));
+        }
+        return maxd;
+    }
+};
