82f6fb6b057e27ef3b5034fd70880134cc0abc5b
OmarShamkh <44472968+OmarShamkh@users.noreply.github.com>
2024-09-06 11:53:47 +0300

    Time: 77 ms (49.22%), Space: 46.6 MB (23.09%) - LeetHub

---
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int equalPairs(vector<vector<int>>& grid) {
+        vector<vector<int>> rows(grid.size());
+        vector<vector<int>> cols(grid.size());
+        map<vector<int> , int> frqrow;
+        map<vector<int> , int> frqcol;
+        for(int r=0; r<grid.size(); r++){
+            for(int c=0; c<grid[r].size(); c++){
+                rows[r].push_back(grid[r][c]);
+            }
+            frqrow[rows[r]]++;
+        }
+        for(int c=0; c<grid.size(); c++){
+            for(int r=0; r<grid[c].size(); r++){
+                cols[c].push_back(grid[r][c]);
+            }
+            frqcol[cols[c]]++;
+        }
+        int ans = 0;                 
+        for(auto x : rows){
+            if(frqcol[x]) ans += frqcol[x];
+        }
+        
+        return ans;
+    }
+};
+// 3 1 2 2
+// 1 4 4 4
+// 2 4 2 2
+// 2 5 2 2
