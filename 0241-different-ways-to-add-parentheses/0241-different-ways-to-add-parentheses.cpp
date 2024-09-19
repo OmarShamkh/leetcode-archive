2a155fe2affca0da4eface1e7b1a1a57c3e9ada2
OmarShamkh <44472968+OmarShamkh@users.noreply.github.com>
2024-09-19 23:52:38 +0300

    Time: 4 ms (45.85%), Space: 13.6 MB (53.55%) - LeetHub

---
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> diffWaysToCompute(string expression) {
+        vector<int> results;
+        bool isNumber = 1;
+        for(int i = 0; i < expression.length(); i++) {
+            if(!isdigit(expression[i])) {
+                isNumber = 0;
+                
+                vector<int> left = diffWaysToCompute(expression.substr(0, i)); 
+                vector<int> right = diffWaysToCompute(expression.substr(i + 1));
+                
+                for(int x : left) {
+                    for(int y : right) {
+                        int val = perform(x, y, expression[i]);
+                        results.push_back(val);
+                    }
+                }
+            }
+        }
+        if(isNumber == 1) results.push_back(stoi(expression));
+        return results;
+    }
+    int perform(int x, int y, char op) {
+        if(op == '+') return x + y;
+        if(op == '-') return x - y;
+        if(op == '*') return x * y;
+        return 0;
+    }
+};
