0bf83839ca5c0b74ec0a0561dc6bb3951a9d180f
OmarShamkh <44472968+OmarShamkh@users.noreply.github.com>
2024-10-08 16:18:34 +0300

    Time: 93 ms (90.99%), Space: 32.7 MB (54.72%) - LeetHub

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minSwaps(string s) {
+        int cnt = 0;
+        for(auto c: s){
+            if(c == '[')  cnt++;
+            else{
+                if(cnt > 0) cnt--;
+            }
+        }
+        return (cnt+1) /2;
+    }
+};
