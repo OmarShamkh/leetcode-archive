4f7dc1479a87de9d447059d6c85af04d8bede7cc
OmarShamkh <44472968+OmarShamkh@users.noreply.github.com>
2025-01-01 16:49:19 +0200

    Time: 4 ms (11.39%), Space: 8.6 MB (61.74%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.cpp
@@ -2,6 +2,24 @@ class Solution {
 public:
     int maxScore(string s) {
         int n = s.size();
+        // 011 1101
+        int ans = 0;
+        for(int i=0; i<n-1; i++){
+            int cnt = 0;
+            for(int j=0; j<=i; j++){
+                if(s[j] == '0') cnt++;
+            }
+            for(int j=i+1; j<n; j++){
+                if(s[j] == '1') cnt++;
+            }
+            ans = max(ans, cnt);
+        }
+        return ans;
+    }
+};
+
+/*
+int n = s.size();
         int pre[n];
         pre[0] = s[0] == '0' ? 0 : 1;
         for(int i=1; i<n; i++){
@@ -16,5 +34,4 @@ public:
             ans = max(ans, cnt_0 + pre[n-1] - pre[i]);
         }
         return ans;
-    }
-};
+*/
