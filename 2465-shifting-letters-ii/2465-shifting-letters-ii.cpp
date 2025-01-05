2b8ce61eb20ec4838b13423609ea3566c0e7583f
OmarShamkh <44472968+OmarShamkh@users.noreply.github.com>
2025-01-05 21:31:42 +0200

    Time: 55 ms (21.83%), Space: 108.3 MB (24.76%) - LeetHub

---
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string shiftingLetters(string s, vector<vector<int>>& shifts) {
+        int n = s.size();
+        vector<int>pre(n+1,0);
+        for(auto x : shifts){
+            int l = x[0], r = x[1], d = x[2];
+            if(d == 1){
+                pre[l] += 1;
+                pre[r+1] -= 1;
+            }
+            else{
+                pre[l] -= 1;
+                pre[r+1] += 1;
+            }
+        }
+        for(int i=1; i<n; i++){
+            pre[i] = pre[i] + pre[i-1];
+        }
+        for(int i=0; i<n; i++){
+            int shift = (pre[i] % 26 + 26) % 26;
+            s[i] = 'a' + (s[i] - 'a' + shift) % 26;
+        }
+        return s;
+    }
+};
